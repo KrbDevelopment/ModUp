cd1d61a7d9c3818cf5bce7ee0103a91d8199b38d
Jan Ruhfus <unknownditx@gmail.com>
2021-03-24 13:46:43 +0100

    Update Level Listener

--- src/events/levelListener.js
+++ src/events/levelListener.js
@@ -1,16 +1,16 @@
 const common = require("../../common");
 const ranking = require("../utils/rankingFunctions");
-console.log("=> Initing discord module: MessageListener...")
+const {logConsole} = require("../utils/logFunctions");
+
+logConsole('Initing discord module: LevelListener...', "EVENT/INFO", "");
 
 common.data['discord'].client.on('message', (message) => {
     if (message.author.bot) return;
-
 
     if (!message.content.startsWith(common.data['config'].BOT_PREFIX)) {
         var guild = message.guild.id
         var userid = message.member.id
 
         ranking.addMemberMessagePoints(guild, userid).then();
-
     }
 });
